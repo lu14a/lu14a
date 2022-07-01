9aab6b756c711a13a6d11112cf7d6a2d9e728f67
lu14a <108016319+lu14a@users.noreply.github.com>
2022-07-01 02:11:34 -0400

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "1812aabd-f3c5-48fc-a453-d256d36e6c6d1",
                         "alterId": 0
                     }
                 ],
